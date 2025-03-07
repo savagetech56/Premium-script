c56e1488f6f44556cf2279987db65e57b8427dd5
savagetech56 <kimarujontez@gmail.com>
2025-03-07 09:54:48 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -30,7 +30,7 @@ fun_bar() {
     tput cnorm
 }
 res1() {
-    wget https://raw.githubusercontent.com/savagetech56/toxicscript/main/toxictech/menu.zip
+    wget https://raw.githubusercontent.com/savagetech56/toxicscript/main/ubuntu/menu.zip
     unzip menu.zip
     chmod +x menu/*
     mv menu/* /usr/local/sbin
