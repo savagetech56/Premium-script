e34df9c934690a01d6d1cc9a916bc2e619fd5d91
savagetech56 <kimarujontez@gmail.com>
2025-03-07 06:58:17 +0000

    Add files via upload

---
+++ kyt.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+NS=$( cat /etc/xray/dns )
+PUB=$( cat /etc/slowdns/server.pub )
+domain=$(cat /etc/xray/domain)
+#color
+grenbo="\e[92;1m"
+NC='\e[0m'
+#install
+apt update && apt upgrade
+apt install python3 python3-pip git
+cd /usr/bin
+wget https://raw.githubusercontent.com/savagetech56/toxicscript/main/ubuntu/bot.zip
+unzip bot.zip
+mv bot/* /usr/bin
+chmod +x /usr/bin/*
+rm -rf bot.zip
+clear
+wget https://raw.githubusercontent.com/savagetech56/toxicscript/main/ubuntu/kyt.zip
+unzip kyt.zip
+pip3 install -r kyt/requirements.txt
+#!/bin/bash
+# Define your password here
+PASSWORD="toxic"
+# Prompt for the password
+echo "TO USE ME PLEASE ENTER PASSWORD 🔑:"
+read -s user_password  # -s flag hides the password input
+# Check if the entered password is correct
+if [ "$user_password" == "toxic" ]; then
+    echo "☺️THANK YOU FOR CHOOSING ME ENJOY🌍"
+    # Your other script content here...
+else
+    echo "Incorrect password. Exiting..."
+    exit 1  # Exit the script if the password is wrong
+fi
+clear;clear;clear
+
+#isi data
+echo ""
+echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e " \e[1;97;101m          ADD BOT PANEL          \e[0m"
+echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "${grenbo}Tutorial Creat Bot and ID Telegram${NC}"
+echo -e "${grenbo}[*] Creat Bot and Token Bot : @BotFather${NC}"
+echo -e "${grenbo}[*] Info Id Telegram : @MissRose_bot , perintah /info${NC}"
+echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+read -e -p "[*] Input your Bot Token : " bottoken
+read -e -p "[*] Input Your Id Telegram :" admin
+echo -e BOT_TOKEN='"'$bottoken'"' >> /usr/bin/kyt/var.txt
+echo -e ADMIN='"'$admin'"' >> /usr/bin/kyt/var.txt
+echo -e DOMAIN='"'$domain'"' >> /usr/bin/kyt/var.txt
+echo -e PUB='"'$PUB'"' >> /usr/bin/kyt/var.txt
+echo -e HOST='"'$NS'"' >> /usr/bin/kyt/var.txt
+clear
+
+cat > /etc/systemd/system/kyt.service << END
+[Unit]
+Description=Simple kyt - @kyt
+After=network.target
+
+[Service]
+WorkingDirectory=/usr/bin
+ExecStart=/usr/bin/python3 -m kyt
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+END
+
+systemctl start kyt 
+systemctl enable kyt
+systemctl restart kyt
+cd /root
+rm -rf kyt.sh
+echo "Done"
+echo "Your Data Bot"
+echo -e "==============================="
+echo "Token Bot         : $bottoken"
+echo "Admin          : $admin"
+echo "Domain        : $domain"
+echo "Pub            : $PUB"
+echo "Host           : $NS"
+echo -e "==============================="
+echo "Setting done"
+clear
+
+echo " Installations complete, type /menu on your bot"
